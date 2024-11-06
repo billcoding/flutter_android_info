0fbced4a279002e7367cfed2717542b8ed69e635
billcoding <wangmiao07@sina.com>
2024-11-06 17:39:31 +0800

    feat: refactor android id using oaid

--- android/src/main/java/com/rwscode/plugins/android_info/FlutterAndroidInfo.java
+++ android/src/main/java/com/rwscode/plugins/android_info/FlutterAndroidInfo.java
@@ -1,30 +1,32 @@
 package com.rwscode.plugins.android_info;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.pm.ActivityInfo;
 import android.content.pm.ApplicationInfo;
 import android.os.Build;
-import android.provider.Settings;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 
+import com.github.gzuliyujiang.oaid.DeviceID;
+
+import java.io.FileInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 
-import java.io.FileInputStream;
-import java.security.MessageDigest;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 /**
  * FlutterAndroidInfo
  */
-public class FlutterAndroidInfo implements FlutterPlugin, MethodCallHandler {
+public class FlutterAndroidInfo implements FlutterPlugin, MethodCallHandler, ActivityAware {
     /// The MethodChannel that will the communication between Flutter and native Android
     ///
     /// This local reference serves to register the plugin with the Flutter Engine and unregister it
@@ -35,44 +37,34 @@ public class FlutterAndroidInfo implements FlutterPlugin, MethodCallHandler {
     private static final String brand = Build.BRAND;
     private static final String model = Build.MODEL;
     private static final String device = Build.DEVICE;
-    private static String buildId = getBuildId("_");
-    private static String androidId = "";
-    private static String deviceId = "";
+    private static String buildId = getBuildId();
     private static String apkMd5 = "";
+    private static Context ctx = null;
 
     @Override
     public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
         channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "flutter_android_info");
         channel.setMethodCallHandler(this);
-        final Context ctx = flutterPluginBinding.getApplicationContext();
-        if (androidId.equals("")) {
-            androidId = Settings.Secure.getString(ctx.getContentResolver(), Settings.Secure.ANDROID_ID);
-        }
-        if (deviceId.equals("")) {
-            deviceId = getDeviceId();
-        }
-        if (apkMd5.equals("")) {
-           apkMd5 = getApkMd5(ctx);
-        }
-        dLog();
+        ctx = flutterPluginBinding.getApplicationContext();
+        apkMd5 = getApkMd5(ctx);
     }
 
-    private static void dLog(){
+    private static void dLog() {
         Log.d("FlutterAndroidInfo", "versionRelease: " + versionRelease);
         Log.d("FlutterAndroidInfo", "brand: " + brand);
         Log.d("FlutterAndroidInfo", "model: " + model);
         Log.d("FlutterAndroidInfo", "device: " + device);
         Log.d("FlutterAndroidInfo", "buildId: " + buildId);
-        Log.d("FlutterAndroidInfo", "androidId: " + androidId);
-        Log.d("FlutterAndroidInfo", "deviceId: " + deviceId);
+        Log.d("FlutterAndroidInfo", "androidId: " + getOAID());
+        Log.d("FlutterAndroidInfo", "deviceId: " + getDeviceId());
         Log.d("FlutterAndroidInfo", "apkMd5: " + apkMd5);
     }
 
     @Override
     public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
         // Android 13
-        switch (call.method){
-            default: 
+        switch (call.method) {
+            default:
                 result.notImplemented();
                 break;
             case "versionRelease":
@@ -91,10 +83,10 @@ public class FlutterAndroidInfo implements FlutterPlugin, MethodCallHandler {
                 result.success(buildId);
                 break;
             case "androidId":
-                result.success(androidId);
+                result.success(getOAID());
                 break;
             case "deviceId":
-                result.success(deviceId);
+                result.success(getDeviceId());
                 break;
             case "apkMd5":
                 result.success(apkMd5);
@@ -108,10 +100,10 @@ public class FlutterAndroidInfo implements FlutterPlugin, MethodCallHandler {
     }
 
     private static String getDeviceId() {
-        return Base64.getEncoder().encodeToString((buildId + "_" + androidId).getBytes());
+        return Base64.getEncoder().encodeToString((md5(buildId) + getOAID()).getBytes());
     }
 
-    private static String getBuildId(String delimiter) {
+    private static String getBuildId() {
         List<String> list = new ArrayList<>();
         list.add(brand);
         list.add(model);
@@ -120,7 +112,11 @@ public class FlutterAndroidInfo implements FlutterPlugin, MethodCallHandler {
             list.add(Build.SOC_MANUFACTURER);
             list.add(Build.SOC_MODEL);
         }
-        return String.join(delimiter, list);
+        return String.join("_", list);
+    }
+
+    private static String getOAID() {
+        return md5(DeviceID.getOAID());
     }
 
     public static String getApkMd5(Context ctx) {
@@ -151,16 +147,47 @@ public class FlutterAndroidInfo implements FlutterPlugin, MethodCallHandler {
         return apkMd5;
     }
 
-    private static final char[] hexArray = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+    private static String md5(String str) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(str.getBytes());
+            return byteArrayToHexString(md.digest()).toLowerCase();
+        } catch (NoSuchAlgorithmException ex) {
+            Log.d("FlutterAndroidInfo", "error：" + ex.getMessage());
+        }
+        return "";
+    }
+
+    private static final char[] hexArray = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 
     private static String byteArrayToHexString(byte[] bytes) {
-        char[] hexChars = new char[bytes.length*2];
+        char[] hexChars = new char[bytes.length * 2];
         int v;
-        for(int j=0; j < bytes.length; j++) {
+        for (int j = 0; j < bytes.length; j++) {
             v = bytes[j] & 0xFF;
-            hexChars[j*2] = hexArray[v>>>4];
-            hexChars[j*2 + 1] = hexArray[v & 0x0F];
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
         }
         return new String(hexChars);
+    }
+
+    @Override
+    public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
+        DeviceID.register(binding.getActivity().getApplication());
+    }
+
+    @Override
+    public void onDetachedFromActivityForConfigChanges() {
+
+    }
+
+    @Override
+    public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
+
+    }
+
+    @Override
+    public void onDetachedFromActivity() {
+
     }
 }
